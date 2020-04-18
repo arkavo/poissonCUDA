b535cacdcf60da9327c422c852305ce003573fa3
Arkavo Hait <arkavohait@gmail.com>
2020-04-19 00:51:57 +0530

    +1

--- dataRead.py
+++ dataRead.py
@@ -4,13 +4,13 @@ import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
 
 data = np.loadtxt('data.txt')
-new_data = data.reshape(8,8)
+new_data = data.reshape(256,256)
 #kde = stats.gaussian_kde(new_data)
 print(new_data)
-x = np.linspace(0,7,8)
-y = np.linspace(0,7,8)
+x = np.linspace(0,255,256)
+y = np.linspace(0,255,256)
 X,Y = np.meshgrid(x,y)
 ax = plt.axes(projection='3d')
-ax.contour3D(X, Y, new_data, 50, cmap='binary')
+ax.contour3D(X, Y, new_data, 255, cmap='viridis')
 #plt.plot(new_data)
 plt.show()
