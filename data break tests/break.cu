e473a95d720e381c6182756a3240df705d4fa7fd
Arkavo Hait <arkavohait@gmail.com>
2020-05-19 16:50:47 +0530

    changes

--- data break tests/break.cu
+++ data break tests/break.cu
@@ -6,7 +6,6 @@
 #include <chrono>
 #include <string.h>
 #include <fstream>
-#include <vector>
 
 #define RANGE 512
 
@@ -37,17 +36,16 @@ __device__ int getGlobalIdx_3D_3D()
 void printDevProp(cudaDeviceProp devProp)
 {   
     printf("%s\n", devProp.name);
-    printf("Major revision number:         %d\n", devProp.major);
-    printf("Minor revision number:         %d\n", devProp.minor);
-    printf("Total global memory:           %zu", devProp.totalGlobalMem);
+    printf("Major revision number:                   %d\n", devProp.major);
+    printf("Minor revision number:                   %d\n", devProp.minor);
+    printf("Total global memory:                     %zu", devProp.totalGlobalMem);
     printf("bytes\n");
-    printf("Number of multiprocessors:     %d\n", devProp.multiProcessorCount);
+    printf("Number of multiprocessors:               %d\n", devProp.multiProcessorCount);
     printf("Total amount of shared memory per block: %zu\n",devProp.sharedMemPerBlock);
-    printf("Total registers per block:     %d\n", devProp.regsPerBlock);
-    printf("Warp size:                     %d\n", devProp.warpSize);
-    printf("Maximum memory pitch:          %zu\n", devProp.memPitch);
+    printf("Total registers per block:               %d\n", devProp.regsPerBlock);
+    printf("Warp size:                               %d\n", devProp.warpSize);
+    printf("Maximum memory pitch:                    %zu\n", devProp.memPitch);
     printf("Total amount of constant memory:         %zu\n",   devProp.totalConstMem);
-    return;
 }
 
 void value(int mode, float val)
@@ -68,7 +66,11 @@ void value(int mode, float val)
 
 int main()
 {
+    //get Devices
     int nDevices;
+    /*Hard Set n
+    int nDevices = 1;
+    */
     cudaGetDeviceCount(&nDevices);
     //Device list and properties
     for (int i = 0; i < nDevices; i++) 
@@ -80,9 +82,12 @@ int main()
         printf("Device name: %s\n", prop.name);
         printf("Memory Clock Rate (KHz): %d\n",prop.memoryClockRate);
         printf("Memory Bus Width (bits): %d\n",prop.memoryBusWidth);
-        printf("  Peak Memory Bandwidth (GB/s): %f\n\n",2.0*prop.memoryClockRate*(prop.memoryBusWidth/8)/1.0e6);
+        printf("Peak Memory Bandwidth (GB/s): %f\n\n",2.0*prop.memoryClockRate*(prop.memoryBusWidth/8)/1.0e6);
     }
     //end Device list
-    return 0;
-    
+    //return 0;
+    void* ADDRESS_LIST;
+    ADDRESS_LIST = (void*)malloc(sizeof(int)*nDevices*nDevices*nDevices);
+    ADDRESS_LIST = DATA;
+    printf("%p\n",ADDRESS_LIST);
 }
