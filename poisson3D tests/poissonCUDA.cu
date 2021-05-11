9c6cc36401b1232e36367ad0091f69417cf7712f
Arkavo Hait <arkavohait@gmail.com>
2021-05-12 02:03:22 +0530

    final_code

--- poisson3D tests/poissonCUDA.cu
+++ poisson3D tests/poissonCUDA.cu
@@ -1,3 +1,7 @@
+//Three Dimensional Poisson solver
+//Author: Arkavo Hait, 2021
+
+
 #include <stdio.h>
 #include <iostream>
 #include <cmath>
@@ -5,10 +9,13 @@
 
 using namespace std;
 
+//Define Global variables
+
 #define X 32
 #define Y 32
 #define Z 32
 
+//X derivative (unused)
 __global__ void DX(double* R, double* C,double dx)
 {
     int index = threadIdx.x*X + blockIdx.x*X*Y +X+X*Y;
@@ -17,7 +24,7 @@ __global__ void DX(double* R, double* C,double dx)
         *(R+index+i) = (*(C+index+1+i) - *(C+index-1+i))/(dx*2);
     }
 }
-
+//Y derivative (unused)
 __global__ void DY(double* R, double* C,double dy)
 {
     int index = threadIdx.x + blockIdx.x*X*Y+ X + X*Y;
@@ -26,7 +33,7 @@ __global__ void DY(double* R, double* C,double dy)
         *(R+index+i) = (*(C+index+X*(i+1)) - *(C+index+X*(i-1)))/(dy*2);
     }
 }
-
+//Z derivative (unused)
 __global__ void DZ(double* R, double* C,double dz)
 {
     int index = threadIdx.x + blockIdx.x*X+X+X*Y;
@@ -36,6 +43,7 @@ __global__ void DZ(double* R, double* C,double dz)
     }
 }
 
+//X double derivative
 __global__ void DDX(double* R, double* C,double dx)
 {
     int index = threadIdx.x*X + blockIdx.x*X*Y + X+X*Y;
@@ -45,6 +53,7 @@ __global__ void DDX(double* R, double* C,double dx)
     }
 }
 
+//Y double derivative
 __global__ void DDY(double* R, double* C,double dy)
 {
     int index = threadIdx.x + blockIdx.x*X*Y+X+X*Y;
@@ -54,6 +63,7 @@ __global__ void DDY(double* R, double* C,double dy)
     }
 }
 
+//Z double derivative
 __global__ void DDZ(double* R, double* C,double dz)
 {
     int index = threadIdx.x + blockIdx.x*X+X+X*Y;
@@ -63,6 +73,7 @@ __global__ void DDZ(double* R, double* C,double dz)
     }
 }
 
+//parallel function to update matrices
 __global__ void ASSIGN(double* R, double* C)
 {
     int index = threadIdx.x*X + blockIdx.x*X*Y + X + X*Y ;
@@ -72,6 +83,7 @@ __global__ void ASSIGN(double* R, double* C)
     }
 }
 
+//parallel function to add two matrices
 __global__ void ADD(double* R,double* C,double dt)
 {
     int index = threadIdx.x*X+blockIdx.x*X*Y+X+X*Y;
@@ -81,6 +93,7 @@ __global__ void ADD(double* R,double* C,double dt)
     }
 }
 
+//parallel function to compare two matrices, outputting a maximum difference bteween elements
 __global__ void COMPARE(double* R, double* C, double* OUT_H)
 {
     int index = threadIdx.x * X + blockIdx.x * X * Y+X+X*Y;
@@ -92,10 +105,14 @@ __global__ void COMPARE(double* R, double* C, double* OUT_H)
         }
     }
 }
+
+//reset function, use it to reset any pointer
 __global__ void RESET_CTR(double* C)
 {
     *C = 0;
 }
+
+//display function, use to disaply any array
 void display(double* DATA)
 {
     for(int i=0;i<X;i++)
@@ -104,6 +121,8 @@ void display(double* DATA)
                 {printf("%.2lf ",*(DATA+i+X*j+X*Z*k));}
             printf("\n");}printf("\n");}
 }
+
+//main fxn, will fix with args after
 int main()
 {
     double DATA_H[X][Y][Z];
@@ -117,95 +136,86 @@ int main()
                         DATA_H[i][j][k] = 0;
     }}}
     
+    //step for double derivatives
     double step = 0.01;
+    //error counters
     double* CC;
     double CCD = 0;
-
+    //tolerence
     double tol = 0.00001;
-
+    //DATA pointer for device state 0
     double* DATA_ORIGINAL;
-    //double* DATA_TRIMMED;
+    //DATA pointer for device state 1
     double* DATA_NEXT;
-    double* DX_D;
-    double* DY_D;
-    double* DZ_D;
+    //Derivative results pointers
     double* DDX_D;
     double* DDY_D;
     double* DDZ_D;
     
+    //array size for device
     unsigned long long SIZE_0 = ((int)sizeof(double))*X*Y*Z;
-    //unsigned long long SIZE_1 = sizeof(double)*(X-2)*(Y-2)*(Z-2);
-    
+    //allocating space for arrays
     cudaMalloc((void**)&DATA_ORIGINAL,SIZE_0);
-    //cudaMalloc(&DATA_TRIMMED,SIZE_1);
+    
     cudaMalloc((void**)&DATA_NEXT,SIZE_0);
     
-    cudaMalloc((void**)&DX_D,SIZE_0);
-    cudaMalloc((void**)&DY_D,SIZE_0);
-    cudaMalloc((void**)&DZ_D,SIZE_0);
     cudaMalloc((void**)&DDX_D,SIZE_0);
     cudaMalloc((void**)&DDY_D,SIZE_0);
     cudaMalloc((void**)&DDZ_D,SIZE_0);
     
     cudaMalloc(&CC,(int)sizeof(double));
-    //cudaMemcpy(CC,&CCD,sizeof(double),cudaMemcpyHostToDevice);
+    
+    //token counter
     CCD = 10;
-
+    //copy data state0, state1 
     cudaMemcpy(DATA_ORIGINAL,&DATA_H,SIZE_0,cudaMemcpyHostToDevice);
     cudaMemcpy(DATA_NEXT,&DATA_H,SIZE_0,cudaMemcpyHostToDevice);
+    //data arrays for final copies
     double DATA_F[X][Y][Z];
-    double DATA_FF[X][Y][Z];
     
-    cudaMemcpy(&DATA_FF,DATA_ORIGINAL,SIZE_0,cudaMemcpyDeviceToHost);
-    //display(&DATA_FF[0][0][0]);
-    //ASSIGN<<<Y,Z>>>(DATA_TRIMMED,DATA_ORIGINAL);
+    //double DATA_FF[X][Y][Z];
+    
+    //cudaMemcpy(&DATA_FF,DATA_ORIGINAL,SIZE_0,cudaMemcpyDeviceToHost);
+    
+    //counter
     int ct = 0;
-    //ADD <<<Y,Z>>> (DATA_NEXT,DATA_ORIGINAL,1);
+    
+    //run while tolerence > difference
     while(CCD>=tol)
     {
+        //reset difference every loop
         RESET_CTR  <<<1,1>>>  (CC);
-        //printf("%lf\r",*CC);
-        //DX <<<Y-2,Z-2>>> (DX_D,DATA_ORIGINAL,1.);
-        //DY <<<Z-2,X-2>>> (DY_D,DATA_ORIGINAL,1.);
-        //DZ <<<Z-2,X-2>>> (DZ_D,DATA_ORIGINAL,1.);
-
+        //run derivatives
         DDY <<<Z-2,X-2>>> (DDY_D,DATA_ORIGINAL,10.);
         DDZ <<<X-2,Y-2>>> (DDZ_D,DATA_ORIGINAL,10.);
         DDX <<<Y-2,Z-2>>> (DDX_D,DATA_ORIGINAL,10.);
-        
-        //ADD <<<Y-2,Z-2>>> (DATA_NEXT,DX_D,-1*step);        
-        //ADD <<<Y-2,Z-2>>> (DATA_NEXT,DY_D,-1*step);
-        //ADD <<<Y-2,Z-2>>> (DATA_NEXT,DZ_D,-1*step);
+        //add into state 1
         ADD <<<Y-2,Z-2>>> (DATA_NEXT,DDX_D,step);
         ADD <<<Y-2,Z-2>>> (DATA_NEXT,DDY_D,step);
         ADD <<<Y-2,Z-2>>> (DATA_NEXT,DDZ_D,step);
-        
+        //compare state1 state 0
         COMPARE<<<Y-2,Z-2>>>(DATA_ORIGINAL,DATA_NEXT,CC);
-        
+        //copy back max error
         cudaMemcpy(&CCD,CC,sizeof(double),cudaMemcpyDeviceToHost);
-        //printf("%0.3lf\r",CCD);
-        
+        //make state 1 as state0
         ASSIGN  <<<Y-2,Z-2>>>  (DATA_ORIGINAL,DATA_NEXT);
-        
+        //update counter
         ct += 1;
+        //information every 1000 loops because of visibility
         if(ct%1000==0)
         {
             cudaMemcpy(&CCD,CC,sizeof(double),cudaMemcpyDeviceToHost);
             printf("%d loops %0.6lf max error\r",ct,CCD);
         }
-        //*CC = 0;
     }
-    //double DATA_F[X][Y][Z];
+    //copy back final array
     cudaMemcpy(DATA_F,DATA_ORIGINAL,SIZE_0,cudaMemcpyDeviceToHost);
+    //final print statement
     printf("\nConverged in %d loops\n",ct-1);
     printf("%d\n",SIZE_0);
-    //display(&DATA_F[0][0][0]);
+    //free pointerss
     cudaFree(DATA_ORIGINAL);
-    //cudaFree(DATA_TRIMMED);
     cudaFree(DATA_NEXT);
-    cudaFree(DX_D);
-    cudaFree(DY_D);
-    cudaFree(DZ_D);
     cudaFree(DDX_D);
     cudaFree(DDY_D);
     cudaFree(DDZ_D);
