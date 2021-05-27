e4ceec381003ca6758b3ccb597cdd84b444136cb
Arkavo Hait <arkavohait@gmail.com>
2021-05-28 04:20:57 +0530

    trials

--- src/run.py
+++ src/run.py
@@ -1,9 +1,7 @@
 import subprocess as sp
 
 threads = [1,2,4,8]
-data = [10,15,20,25,30]
+data = [10]
 for i in threads:
     for j in data:
-        for k in data:
-            for l in data:
-                sp.call(["./pCUDA.exe",str(i),str(j),str(k),str(l)])
+        sp.call(["./pCUDA.exe",str(i),str(j),str(j),str(j)])
